bfdb6fc20c5b60c77fe1b0f0f1dacee8013832b0
Ericka <eramos1002@gmail.com>
2021-01-02 21:23:58 +0100

    normalizando cuenta y document

--- src/domain/account.js
+++ src/domain/account.js
@@ -1,25 +1,24 @@
-import ibanValidator from '../tools/iban-validator';
-import documentValidator from '../tools/document-validator';
+import ibanValidator from "../tools/iban-validator";
+import documentValidator from "../tools/document-validator";
 
 export default class Account {
-
     constructor(iban, document) {
+        const normalizedIban = ibanValidator.normalize(iban);
+        const normalizedDocument = documentValidator.normalize(document);
 
+        if (!ibanValidator.isValid(normalizedIban)) {
+            throw new Error(`IBAN ${iban} is invalid`);
+        }
+        if (!normalizedDocument.isValid(documentValidator)) {
+            throw new Error(`IBAN ${iban} is invalid`);
+        }
     }
 
-    open() {
+    open() {}
 
-    }
+    close() {}
 
-    close() {
+    deposit(amount) {}
 
-    }
-
-    deposit(amount) {
-
-    }
-
-    withdrawal(amount) {
-
-    }
+    withdrawal(amount) {}
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log("funciona");
+class Account {
+    constructor(iban, document) {
+        this.iban = iban;
+        this.document = document;
+        this.balance = 0; // saldo
+        this.status = "open";
+    }
+    open() {
+        if (this.status === "close") {
+            this.status = "open";
+        }
+    }
+    close() {
+        if (this.status === "open") {
+            this.status = "close";
+        }
+    }
+    deposit(amount) {
+        if (this.status === "close") {
+            throw new Error("La cuenta esta cerrada");
+        }
+        if (isNaN(amount) || amount <= 0) {
+            throw new Error("El importe debe ser una cantidad positiva");
+        }
+        this.balance += amount;
+    }
+    withdraw(amount) {
+        if (this.status === "close") {
+            throw new Error("La cuenta esta cerrada");
+        }
+        if (isNaN(amount) || amount <= 0 || amount > this.balance) {
+            throw new Error(
+                "El importe debe ser una cantidad positiva y menor que el saldo"
+            );
+        }
+        this.balance -= amount;
+    }
+
+    getBalance() {
+        if (this.status === "close") {
+            throw new Error("La cuenta esta cerrada");
+        }
+        return this.balance;
+    }
+}
+class Bank {
+    constructor() {
+        this.accounts = {};
+    }
+    addAccount(account) {
+        // falta especificar si no es valido y dni
+        if (typeof this.accounts[account] === undefined) {
+            throw new Error("La cuenta no es valida");
+        }
+        this.accounts[account.iban] = account;
+    }
+    getAccountbyIban(Iban) {}
+
+    findAccountbyDocument(document) {}
+}
